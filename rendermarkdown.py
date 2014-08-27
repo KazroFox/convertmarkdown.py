a23cbd71baedba5376b1d387f543c7f2d0527c21
Austin Holbrook <ausholbrook@gmail.com>
2014-08-27 17:55:01 -0400

    Added argument handling logic
    Handles all available flags displayed when ran with -h now.
    Running with -vv is debug mode, and will output stored vars
    
    Program runs, but doesn't do anything besides find the files so far.

--- rendermarkdown.py
+++ rendermarkdown.py
@@ -6,34 +6,56 @@ import os
 import sys
 import argparse
 
+def appendRenderList(l, f, extensions):
+    if os.path.splitext(f)[1] in extensions:
+        return l + [f]
+    return l
+
 def renderFile(inputPath, outputPath, pandoc, pandocFormatArgs):
-    os.system(pandoc + " " pandocFormatArgs + " -o " + outputPath + " " +
+    os.system(pandoc + " " + pandocFormatArgs + " -o " + outputPath + " " +
             inputPath)
 
 def renderFolder(inputDir, outputDir, recursive, extensions, pandoc,
         pandocFormatArgs):
 
+    renderList = []
+    topDirs = []
+    for f in os.listdir(inputDir):
+        if os.path.isfile(os.path.join(inputDir, f)):
+            renderList = appendRenderList(renderList,
+                    os.path.join(inputDir, f), extensions)
+        elif os.path.isdir(os.path.join(inputDir, f)):
+            topDirs.append(os.path.join(inputDir, f))
 
     if recursive:
-        for root, dirs, files in os.walk(inputDir):
-            for f in files:
-                filename, ext = os.path.splitext(os.path.join(root, f))
-                relfilename = os.path.relpath(filename+ext,
-                        start=inputDir)
-                if ext in extensions:
-                    renderFile(filename + ext,
-                            outputDir + relfilename + ".html", pandoc,
-                            pandocFormatArgs)
+        for d in topDirs:
+            for root, dirs, files in os.walk(d):
+                for f in files:
+                    renderList = appendRenderList(renderList,
+                            os.path.join(root, f), extensions)
+
+    print("Files found:")
+    print(renderList)
+                    #filename, ext = os.path.splitext(os.path.join(root, f))
+                    #relfilename = os.path.relpath(filename+ext,
+                    #        start=inputDir)
+                    #if ext in extensions:
+                    #    renderFile(filename + ext,
+                    #            outputDir + relfilename + ".html", pandoc,
+                    #            pandocFormatArgs)
 
 # parser and cli arguments
 parser = argparse.ArgumentParser(description="render all markdown files in a\
         given directory to HTML using pandoc")
+parser.add_argument("-4", "--html4", help="use HTML4 not HTML5 in output",
+        action="store_const", const="html4", default="html5", dest="html")
 parser.add_argument("-e", "--extensions",
-        help="list of extensions that are interpreted as markdown",
-        default=[".md", ".mdown", ".mkd", ".markdown"], action="append_const",
-        const="str")
-parser.add_argument("-l", "--location", help="directory of files to render",
-        metavar="PATH", required=True)
+        help="list of extensions that are interpreted as markdown. default =\
+                .md, .mdown, .mkd, .markdown", action="append", type=str)
+parser.add_argument("-m", "--markdown", help="flavor of markdown to use. \
+        default = github",
+        choices=["markdown", "github", "mmd", "phpextra", "strict"],
+        default="github", type=str)
 parser.add_argument("-o", "--output",
         help="specify alternate output directory. keeps structure intact",
         metavar="PATH")
@@ -46,16 +68,42 @@ parser.add_argument("-s", "--stylesheet", help="location of css file.",
 parser.add_argument("-t", "--toc", help="add table of contents to each file",
         action="store_true")
 parser.add_argument("-v", "--verbosity", action="count",
-        help="increase command verbosity")
-
+        help="more verbose output. option can be supplied several times for \
+                more verbosity.", default=0)
+parser.add_argument("path", help="directory of files to render",
+        metavar="PATH")
 args = parser.parse_args()
 
+#Set default output path to the same input path
+args.output = args.output or args.path
+args.extensions = args.extensions or [".md", ".mdown", ".mkd", ".markdown"]
+
 # pandoc formatting arguments
-pandocFormatArgs = "-r markdown_github -w html5 -s --toc -c http://eku.tau.bz/notes/css/main.css"
+formatArgs = "-r "
+if args.markdown == "markdown":
+    formatArgs += "markdown"
+else:
+    formatArgs += "markdown_" + args.markdown
+formatArgs      += " -w " + args.html
+formatArgs      += " -s"
+if args.toc:
+    formatArgs  += " --toc"
+if args.stylesheet:
+    formatArgs  += " -c " + args.stylesheet
 
-for root, dirs, files in os.walk(args.location):
-    for f in files:
+# Debug
+if args.verbosity >= 2:
+    print("args.extensions = ", args.extensions)
+    print("formatArgs = " + formatArgs)
 
+renderFolder(
+        args.path,
+        args.output,
+        args.recursive,
+        args.extensions,
+        args.pandoc,
+        formatArgs
+)
 
 
 print("done")
